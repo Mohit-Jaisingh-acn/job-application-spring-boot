097dd905454818e9b2ea3d319e3ca4cba2c888fb
Mohit Jaisingh <mohitmjaisingh@gmail.com>
2024-05-01 12:31:49 +0530

    Updated Company Controller

--- src/main/java/com/embark/jobapp/company/CompanyController.java
+++ src/main/java/com/embark/jobapp/company/CompanyController.java
@@ -1,12 +1,10 @@
 package com.embark.jobapp.company;
 
-import com.embark.jobapp.company.impl.CompanyServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
